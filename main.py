361c765c294d36ebde48dce0bacd0bf89cbaab23
igorsheyko1976 <sheyko-iv@mail.ru>
2024-06-29 00:00:29 +0300

    Initial commit

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
 import asyncio
 import aiohttp
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, F
 from aiogram.filters import CommandStart, Command
-from aiogram.types import Message
-
+from aiogram.types import Message, FSInputFile
+from googletrans import Translator
 from config import TOKEN, OPENWEATHERMAP_API_KEY
+
+import random
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
+translator = Translator()
+
+@dp.message(Command('voice'))
+async def voice(message: Message):
+    await bot.send_chat_action(message.chat.id, 'upload_voice')
+    voice = FSInputFile("sample.ogg")
+    await message.answer_voice(voice)
+
+@dp.message(F.photo)
+async def react_photo(message: Message):
+    list = ['Ого, какая фотка!', 'Непонятно, что это такое', 'Не отправляй мне такое больше']
+    rand_answ = random.choice(list)
+    await message.answer(rand_answ)
+    await bot.download(message.photo[-1], destination=f'img/{message.photo[-1].file_id}.jpg')
 
 async def get_weather():
     url = f"http://api.openweathermap.org/data/2.5/weather?q=Novorossiysk&appid={OPENWEATHERMAP_API_KEY}&units=metric&lang=ru"
@@ -47,8 +63,14 @@ async def help(message: Message):
 async def start(message: Message):
     await message.answer("Привет, я бот, который показывает погоду в Новороссийске!")
 
+@dp.message(F.text)
+async def translate_text(message: Message):
+    translated = translator.translate(message.text, dest='en')
+    await message.answer(f"Перевод: {translated.text}")
+
 async def main():
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
     asyncio.run(main())
+
